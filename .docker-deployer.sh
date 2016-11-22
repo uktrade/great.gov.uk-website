ed58a139890a83ff24c21f2381aafc58a5bd001f
breneser <brhneser@gmail.com>
2016-11-22 15:35:12 +0000

    Show docker image information before and after deployment

--- .docker-deployer.sh
+++ .docker-deployer.sh
@@ -6,12 +6,14 @@
 
 DOCKER_IMAGE="$DOCKER_IMAGE_NAME-deployer"
 
+$DOCKER_PATH images && \
 $DOCKER_PATH build --force-rm -t $DOCKER_IMAGE $PWD  && \
 $DOCKER_PATH run --rm -i --name $DOCKER_IMAGE  -e BUCKET=$BUCKET -e DID=$DID -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e NO_ROBOTS=$NO_ROBOTS  $DOCKER_IMAGE /bin/bash -c "npm run deploy"
 
 
 result=$?
 
+$DOCKER_PATH images && \
 $DOCKER_PATH rmi --force $DOCKER_IMAGE
 
 if [ $result -ne 0 ]; then
